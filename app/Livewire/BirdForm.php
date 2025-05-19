1893f918e90bc13f0feaddba3b9b5e7d89b9aae9
tarequr <tarequrr8@gmail.com>
2025-05-19 22:19:37 +0600

    chore: validation

--- app/Livewire/BirdForm.php
+++ app/Livewire/BirdForm.php
@@ -3,15 +3,24 @@
 namespace App\Livewire;
 
 use App\Models\Entry;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class BirdForm extends Component
 {
+    #[Validate('required|integer')]
     public int $count;
+
+    #[Validate('required|string')]
     public string $notes;
 
     public function addBird()
     {
+        $this->validate([
+            'count' => 'required|integer',
+            'notes' => 'required|string'
+        ]);
+
         Entry::create([
             'bird_count' => $this->count,
             'notes' => $this->notes
@@ -22,6 +31,8 @@ class BirdForm extends Component
 
     public function render()
     {
-        return view('livewire.bird-form');
+        return view('livewire.bird-form', [
+            'entries' => Entry::all()
+        ]);
     }
 }
--- resources/views/livewire/bird-form.blade.php
+++ resources/views/livewire/bird-form.blade.php
@@ -1,11 +1,26 @@
 <div>
     <form wire:submit.prevent="addBird">
         <div>
-            <input type="number" wire:model="count">
-            <textarea name="" id="" cols="30" rows="10" wire:model="notes"></textarea>
+            <label for="count">Bird Count</label>
+            <input type="number" wire:model="count" id="count" placeholder="Enter a count">
+        </div>
+
+        <div>
+            <label for="notes">Notes</label>
+            <textarea name="" id="notes" cols="30" rows="10" wire:model="notes" placeholder="Enter notes"></textarea>
         </div>
         <br>
 
         <button>Add a New Bird Entry</button>
     </form>
+
+    <div>
+        @foreach ($entries as $entry)
+            <div>
+                <p>Count: {{ $entry['bird_count'] }}</p>
+                <p>Notes: {{ $entry['notes'] }}</p>
+            </div>
+            <br>
+        @endforeach
+    </div>
 </div>
